c9f2b8a3f14bb0335280ee62981cc8f793862ba3
gorelovys <gorelov.ys@mail.ru>
2021-09-07 15:12:25 +0300

    My first homework

---
+++ src/main/java/HomeWorkApp.java
@@ -0,0 +1,45 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 48;
+        int b = -34;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 121;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <=100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 782;
+        int b = 233;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
